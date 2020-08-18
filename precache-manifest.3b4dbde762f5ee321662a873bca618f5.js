49b97f1992744d4efb8fe3d5a32db8f7c561a688
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-08-18 13:08:47 +0800

    Updates

---
+++ precache-manifest.3b4dbde762f5ee321662a873bca618f5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de7e0164c020b202fa5bb56da7274f1",
+    "url": "/fubang/index.html"
+  },
+  {
+    "revision": "4f73ed40263874816cc0",
+    "url": "/fubang/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c8f4df9a40368ba8dd0d",
+    "url": "/fubang/static/css/main.4377a1ba.chunk.css"
+  },
+  {
+    "revision": "4f73ed40263874816cc0",
+    "url": "/fubang/static/js/2.8b03234f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fubang/static/js/2.8b03234f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f4df9a40368ba8dd0d",
+    "url": "/fubang/static/js/main.8e063e18.chunk.js"
+  },
+  {
+    "revision": "32aee0026df3e811f944",
+    "url": "/fubang/static/js/runtime-main.4da93ac9.js"
+  },
+  {
+    "revision": "72b4544fe6dfbc62ac848afd7cc99c5c",
+    "url": "/fubang/static/media/banner.72b4544f.svg"
+  },
+  {
+    "revision": "919eb0b79f54a6395b2310be19c27f5f",
+    "url": "/fubang/static/media/fb_logo.919eb0b7.svg"
+  },
+  {
+    "revision": "8349f38492f657cc74bdae3df18b5f39",
+    "url": "/fubang/static/media/white_menu_02.8349f384.svg"
+  }
+]);
